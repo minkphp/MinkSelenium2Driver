03a03ef98f611edde2d9da820072d29c1d973af0
Doug Green <douggreen@douggreenconsulting.com>
2013-04-09 09:38:23 -0400

    fire change event when radio changes

--- src/Behat/Mink/Driver/Selenium2Driver.php
+++ src/Behat/Mink/Driver/Selenium2Driver.php
@@ -715,6 +715,12 @@ if (node.tagName == 'SELECT') {
             node.checked = true;
         }
     }
+    if (node.tagName == 'INPUT') {
+      var type = node.getAttribute('type');
+      if (type == 'radio') {
+        triggerEvent(node, 'change');
+      }
+    }
 }
 JS;
 
