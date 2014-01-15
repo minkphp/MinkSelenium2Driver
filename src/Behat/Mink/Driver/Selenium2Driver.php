a273c289beddf5e0560ce6954fde2c107387fca2
Rob Squires <rob.squires@gmail.com>
2014-01-15 00:30:49 +0000

    now using native selenium call for mouseOver

--- src/Behat/Mink/Driver/Selenium2Driver.php
+++ src/Behat/Mink/Driver/Selenium2Driver.php
@@ -848,8 +848,9 @@ JS;
      */
     public function mouseOver($xpath)
     {
-        $script = 'Syn.trigger("mouseover", {}, {{ELEMENT}})';
-        $this->withSyn()->executeJsOnXpath($xpath, $script);
+        $this->wdSession->moveto(
+            array("element" => $this->wdSession->element('xpath', $xpath)->getId())
+        );
     }
 
     /**
