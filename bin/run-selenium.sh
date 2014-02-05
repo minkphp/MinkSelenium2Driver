cc6273740e04b5b05c07d01e7249e1aa175342a2
Christophe Coevoet <stof@notk.org>
2014-02-05 15:20:45 +0100

    Fixed the redirection of the error output

--- bin/run-selenium.sh
+++ bin/run-selenium.sh
@@ -6,4 +6,4 @@ export DISPLAY=:99.0
 sleep 4
 
 curl http://selenium.googlecode.com/files/selenium-server-standalone-2.37.0.jar > selenium.jar
-java -jar selenium.jar > /dev/null 2 > /tmp/webdriver_output.txt &
+java -jar selenium.jar > /dev/null 2> /tmp/webdriver_output.txt &
