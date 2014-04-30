6975bd5bdaa7cd328c0ea33b838fd5a81bfad308
Christophe Coevoet <stof@notk.org>
2014-04-30 15:48:39 +0200

    Updated selenium to its latest version

--- bin/run-selenium.sh
+++ bin/run-selenium.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env sh
 set -e
+set -x
 
 sh -e /etc/init.d/xvfb start
 export DISPLAY=:99.0
 sleep 4
 
-curl http://selenium.googlecode.com/files/selenium-server-standalone-2.37.0.jar > selenium.jar
+curl -L selenium-release.storage.googleapis.com/2.41/selenium-server-standalone-2.41.0.jar > selenium.jar
 java -jar selenium.jar > /dev/null 2> /tmp/webdriver_output.txt &
