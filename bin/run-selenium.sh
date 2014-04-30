ff02645aff5649f07f467dd95a99ee08a694595a
Christophe Coevoet <stof@notk.org>
2014-04-30 16:11:31 +0200

    Readded the schem for the selenium link and improved output

--- bin/run-selenium.sh
+++ bin/run-selenium.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env sh
 set -e
-set -x
 
+echo '    Starting XVFB'
 sh -e /etc/init.d/xvfb start
 export DISPLAY=:99.0
 sleep 4
 
-curl -L selenium-release.storage.googleapis.com/2.41/selenium-server-standalone-2.41.0.jar > selenium.jar
+echo '    Downloading selenium'
+curl -L http://selenium-release.storage.googleapis.com/2.41/selenium-server-standalone-2.41.0.jar > selenium.jar
+echo '    Running selenium'
 java -jar selenium.jar > /dev/null 2> /tmp/webdriver_output.txt &
