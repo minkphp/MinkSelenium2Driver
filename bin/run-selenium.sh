e2645f8816a7fca02d2fac4d6af3feb689bc7d90
Christophe Coevoet <stof@notk.org>
2014-09-26 11:46:41 +0200

    Update selenium to the latest version
    This is necessary to support new HTML5 types added in recent versions of
    Firefox.

--- bin/run-selenium.sh
+++ bin/run-selenium.sh
@@ -7,6 +7,6 @@ export DISPLAY=:99.0
 sleep 4
 
 echo '    Downloading selenium'
-curl -L http://selenium-release.storage.googleapis.com/2.41/selenium-server-standalone-2.41.0.jar > selenium.jar
+curl -L http://selenium-release.storage.googleapis.com/2.43/selenium-server-standalone-2.43.1.jar > selenium.jar
 echo '    Running selenium'
 java -jar selenium.jar > /dev/null 2> /tmp/webdriver_output.txt &
