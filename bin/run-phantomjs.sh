1872a6f0dfdae1527834f3bd4c66c7cfc634a83a
Christophe Coevoet <stof@notk.org>
2015-03-22 18:00:03 +0100

    Add testing on PhantomJS 2

--- bin/run-phantomjs.sh
+++ bin/run-phantomjs.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env sh
 set -e
 
+if [ "2" = "$PHANTOM_VERSION" ]; then
+    mkdir travis-phantomjs
+    wget https://s3.amazonaws.com/travis-phantomjs/phantomjs-2.0.0-ubuntu-12.04.tar.bz2 -O $PWD/travis-phantomjs/phantomjs-2.0.0-ubuntu-12.04.tar.bz2
+    tar -xvf $PWD/travis-phantomjs/phantomjs-2.0.0-ubuntu-12.04.tar.bz2 -C $PWD/travis-phantomjs
+    export PATH=$PWD/travis-phantomjs:$PATH
+fi
+
 phantomjs --version
 echo '    Running PhantomJS'
 phantomjs --webdriver=4444 > /tmp/webdriver_output.txt &
