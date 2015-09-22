ade7dc6e53a64c48d8c3d09a658a2f9b8582897d
Christoph Benker <christoph.benker@inserteffect.com>
2015-09-22 14:49:19 +0200

    Clear `input` and `textarea` on `setValue`
    - Send `Delete`- and `Backspace`-Keystrokes for every Character
      in the Input or Textarea
    - Previous behaviour which sent all deletions + given value in
      one String didn't work

--- src/Selenium2Driver.php
+++ src/Selenium2Driver.php
@@ -657,9 +657,15 @@ JS;
 
         if (in_array($elementName, array('input', 'textarea'))) {
             $existingValueLength = strlen($element->attribute('value'));
+            $deleteAndBackspace = Key::DELETE . Key::BACKSPACE;
+
+            for($i = 0; $i < $existingValueLength; $i++) {
+                $element->postValue(array('value' => array($deleteAndBackspace)));
+            }
+
             // Add the TAB key to ensure we unfocus the field as browsers are triggering the change event only
             // after leaving the field.
-            $value = str_repeat(Key::BACKSPACE . Key::DELETE, $existingValueLength) . $value . Key::TAB;
+            $value = $value . Key::TAB;
         }
 
         $element->postValue(array('value' => array($value)));
