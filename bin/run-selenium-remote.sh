f519ea6af6ced849156ccd8eb2da1024ac4a72b9
Pieter Frenssen <pieter@frenssen.be>
2016-10-28 17:55:33 +0300

    Use Firefox instead of Chrome for the remote Selenium instance.

--- bin/run-selenium-remote.sh
+++ bin/run-selenium-remote.sh
@@ -2,6 +2,6 @@
 set -e
 
 echo '    Downloading selenium'
-docker pull selenium/standalone-chrome
+docker pull selenium/standalone-firefox:2.53.1
 echo '    Running selenium'
-docker run -d -p 4444:4444 --network=host selenium/standalone-chrome
+docker run -d -p 4444:4444 --network=host selenium/standalone-firefox:2.53.1
