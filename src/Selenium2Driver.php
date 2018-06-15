e3c59de91a9c797cb5a909436f73ab8243b520ea
Hristo Chonov <hristo.chonov@biologis.com>
2018-06-15 11:59:27 +0200

    Prevent firing the "change" event twice

--- src/Selenium2Driver.php
+++ src/Selenium2Driver.php
@@ -677,13 +677,10 @@ JS;
 
         if (in_array($elementName, array('input', 'textarea'))) {
             $existingValueLength = strlen($element->attribute('value'));
-            // Add the TAB key to ensure we unfocus the field as browsers are triggering the change event only
-            // after leaving the field.
             $value = str_repeat(Key::BACKSPACE . Key::DELETE, $existingValueLength) . $value;
         }
 
         $element->postValue(array('value' => array($value)));
-        $this->trigger($xpath, 'change');
     }
 
     /**
