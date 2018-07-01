2c2dc4ced9f49002cd599dac4a63db0e09880888
Alexander Obuhovich <aik.bold@gmail.com>
2018-07-01 15:56:03 +0300

    Test one PHP version on Chrome

--- bin/run-selenium.sh
+++ bin/run-selenium.sh
@@ -6,6 +6,8 @@ sh -e /etc/init.d/xvfb start
 export DISPLAY=:99.0
 
 echo '    Downloading selenium'
-curl -L http://selenium-release.storage.googleapis.com/2.52/selenium-server-standalone-2.52.0.jar > selenium.jar
+curl -L https://selenium-release.storage.googleapis.com/3.13/selenium-server-standalone-3.13.0.jar > selenium.jar
+curl -L https://chromedriver.storage.googleapis.com/2.40/chromedriver_linux64.zip > chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
 echo '    Running selenium'
 java -jar selenium.jar -log /tmp/webdriver.log > /tmp/webdriver_output.txt 2>&1 &
