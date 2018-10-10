41da6d58679c1adb62d8e8b9820672bc9cf00578
Christophe Coevoet <stof@notk.org>
2018-10-10 14:06:06 +0200

    Use chrome on Travis rather than Firefox for most jobs
    On Trusty, the preinstalled Firefox version is newer than the latest version
    supported by the FirefoxDriver of Selenium 2, and requires using the W3C
    protocol. But we don't support this protocol yet.

--- bin/run-selenium.sh
+++ bin/run-selenium.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 set -e
 
 echo '    Starting XVFB'
@@ -6,6 +6,12 @@ sh -e /etc/init.d/xvfb start
 export DISPLAY=:99.0
 
 echo '    Downloading selenium'
-curl -L http://selenium-release.storage.googleapis.com/2.52/selenium-server-standalone-2.52.0.jar > selenium.jar
+if [ "${SELENIUM_VERSION:-3}" = "2" ]; then
+    curl -L http://selenium-release.storage.googleapis.com/2.53/selenium-server-standalone-2.53.0.jar > selenium.jar
+else
+    curl -L https://selenium-release.storage.googleapis.com/3.14/selenium-server-standalone-3.14.0.jar > selenium.jar
+fi;
+curl -L -O https://chromedriver.storage.googleapis.com/2.42/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
 echo '    Running selenium'
-java -jar selenium.jar -log /tmp/webdriver.log > /tmp/webdriver_output.txt 2>&1 &
+java -Dwebdriver.chrome.driver="$PWD/chromedriver" -jar selenium.jar -log /tmp/webdriver.log > /tmp/webdriver_output.txt 2>&1 &
