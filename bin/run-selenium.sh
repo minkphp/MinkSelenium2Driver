ecd9e465d62854e7be005faaff79b617396abee3
Oleg Andreyev <oleg.andreyev@ecentria.com>
2018-12-01 01:09:24 +0200

    updated run-selenium.sh

--- bin/run-selenium.sh
+++ bin/run-selenium.sh
@@ -6,6 +6,6 @@ sh -e /etc/init.d/xvfb start
 export DISPLAY=:99.0
 
 echo '    Downloading selenium'
-curl -L http://selenium-release.storage.googleapis.com/2.52/selenium-server-standalone-2.52.0.jar > selenium.jar
+curl -L http://selenium-release.storage.googleapis.com/2.53/selenium-server-standalone-2.53.1.jar > selenium.jar
 echo '    Running selenium'
 java -jar selenium.jar -log /tmp/webdriver.log > /tmp/webdriver_output.txt 2>&1 &
