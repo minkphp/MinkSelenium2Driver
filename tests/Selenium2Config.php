e7ad036107c8e8ce6c56da0bd9aab7c2d07a0f75
Oleg Andreyev <oleg@andreyev.lv>
2018-12-09 18:29:47 +0200

    updated phpunit.xml.dist with examples for FF, updated Selenium2Config to support capability from phpunit.xml, updated setRequestHeader to support user-agent

--- tests/Selenium2Config.php
+++ tests/Selenium2Config.php
@@ -3,6 +3,10 @@
 namespace Behat\Mink\Tests\Driver;
 
 use Behat\Mink\Driver\Selenium2Driver;
+use Facebook\WebDriver\Chrome\ChromeOptions;
+use Facebook\WebDriver\Firefox\FirefoxDriver;
+use Facebook\WebDriver\Firefox\FirefoxProfile;
+use Facebook\WebDriver\Remote\DesiredCapabilities;
 
 class Selenium2Config extends AbstractConfig
 {
@@ -20,14 +24,43 @@ class Selenium2Config extends AbstractConfig
         $driverOptions = getenv('DRIVER_OPTIONS') ? \json_decode(getenv('DRIVER_OPTIONS'), true) : array();
         $seleniumHost = $_SERVER['DRIVER_URL'];
 
-        $desiredCapabilities = array(
-            'browser'       => $browser,
-            'browserName'   => $browser,
-            'version'       => 'ANY'
-        );
-        $desiredCapabilities = \array_merge($desiredCapabilities, $driverOptions);
+        if ($browser === 'firefox') {
+            $desiredCapabilities = DesiredCapabilities::firefox();
+        } else if ($browser === 'chrome') {
+            $desiredCapabilities = DesiredCapabilities::chrome();
+        } else {
+            $desiredCapabilities = new DesiredCapabilities();
+        }
 
-        return new Selenium2Driver($browser, $desiredCapabilities, $seleniumHost);
+        $capabilityMap = array(
+            'firefox' => FirefoxDriver::PROFILE,
+            'chrome'  => ChromeOptions::CAPABILITY
+        );
+
+        if (isset($capabilityMap[$browser])) {
+            $capability = $desiredCapabilities->getCapability($capabilityMap[$browser]);
+            if ($capability instanceof ChromeOptions) {
+                $args = isset($driverOptions['args']) ? $driverOptions['args'] : array();
+                $capability->addArguments($args);
+                //$capability->addEncodedExtension();
+                //$capability->addExtension();
+                //$capability->addEncodedExtensions();
+                //$capability->addExtensions();
+            } else if ($capability instanceof FirefoxProfile) {
+                $preferences = isset($driverOptions['preference']) ? $driverOptions['preference'] : array();
+                foreach ($preferences as $key => $preference) {
+                    $capability->setPreference($key, $preference);
+                   // $capability->setRdfFile($key, $preference);
+                   // $capability->addExtensionDatas($key, $preference);
+                   // $capability->addExtension($key, $preference);
+                }
+            }
+        }
+
+        $driver =  new Selenium2Driver($browser, array(), $seleniumHost);
+        $driver->setDesiredCapabilities($desiredCapabilities);
+
+        return $driver;
     }
 
     /**
