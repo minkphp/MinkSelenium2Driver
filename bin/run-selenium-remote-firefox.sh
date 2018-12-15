9dcf5051e37f003a6e232de1fa83950ec30e4f95
Oleg Andreyev <oleg@andreyev.lv>
2018-12-15 18:45:32 +0200

    downgrade firefox to 2.53.1

--- bin/run-selenium-remote-firefox.sh
+++ bin/run-selenium-remote-firefox.sh
@@ -4,4 +4,4 @@ set -e
 echo '    Downloading selenium'
 docker pull selenium/standalone-firefox:latest
 echo '    Running selenium'
-docker run -d -p 4444:4444 --network=host selenium/standalone-firefox:latest
+docker run -d -p 4444:4444 --network=host selenium/standalone-firefox:2.53.1
