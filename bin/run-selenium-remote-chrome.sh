d84acc3ca25b3188f002b0f704c875fe93e55d2a
Oleg Andreyev <oleg@andreyev.lv>
2018-12-31 00:47:21 +0200

    reverted "setRequestHeader", unified key actions

--- bin/run-selenium-remote-chrome.sh
+++ bin/run-selenium-remote-chrome.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env sh
 set -e
 
-echo '    Downloading selenium'
-docker pull selenium/standalone-chrome:latest
 echo '    Running selenium'
 docker run -d -p 4444:4444 --network=host selenium/standalone-chrome:latest
--- bin/run-selenium-remote-firefox.sh
+++ bin/run-selenium-remote-firefox.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env sh
 set -e
 
-echo '    Downloading selenium'
-docker pull selenium/standalone-firefox:latest
 echo '    Running selenium'
+# do not use newer version, firefox is following w3c much faster then drivers
 docker run -d -p 4444:4444 --network=host selenium/standalone-firefox:2.53.1
