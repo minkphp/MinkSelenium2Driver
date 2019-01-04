aa125421e1bacf887b88f02c86a4dfc795588840
Oleg Andreyev <oleg@andreyev.lv>
2019-01-04 14:50:07 +0200

    removed bin/

--- bin/run-selenium-remote-chrome.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-set -e
-
-echo '    Running selenium'
-docker run -d -p 4444:4444 --network=host selenium/standalone-chrome:latest
--- bin/run-selenium-remote-firefox.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-set -e
-
-echo '    Running selenium'
-# do not use newer version, firefox is following w3c much faster then drivers
-docker run -d -p 4444:4444 --network=host selenium/standalone-firefox:2.53.1
