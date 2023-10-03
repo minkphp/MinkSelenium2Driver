89870ce6479518c2f2e5bac45cea0f9a3587d392
Sally Young <git@justa.fish>
2023-10-03 17:28:31 +0100

    PHPStan fixes

--- tests/Selenium2Config.php
+++ tests/Selenium2Config.php
@@ -18,7 +18,7 @@ class Selenium2Config extends AbstractConfig
     public function createDriver(): DriverInterface
     {
         $browser = getenv('WEB_FIXTURES_BROWSER') ?: 'firefox';
-        $seleniumHost = getenv('DRIVER_URL', 'http://localhost:4444/wd/hub');
+        $seleniumHost = !empty(getenv('DRIVER_URL')) ? getenv('DRIVER_URL') : 'http://localhost:4444/wd/hub';
 
         return new Selenium2Driver($browser, null, $seleniumHost);
     }
