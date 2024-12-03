0f020ab5e1fc848f0788e461773948490e40dc52
Alex <aik099@users.noreply.github.com>
2024-12-03 15:28:06 +0200

    Normalize behavior of "getText" method (#404)
    Normalize behavior of "getText" method

--- src/Selenium2Driver.php
+++ src/Selenium2Driver.php
@@ -673,11 +673,11 @@ class Selenium2Driver extends CoreDriver
 
     public function getText(string $xpath)
     {
-        return str_replace(
-            array("\r\n", "\r", "\n"),
+        return trim(str_replace(
+            array("\r\n", "\r", "\n", "\xc2\xa0"),
             ' ',
             $this->executeJsOnXpath($xpath, 'return {{ELEMENT}}.innerText;')
-        );
+        ));
     }
 
     public function getHtml(string $xpath)
